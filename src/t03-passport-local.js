b90768a5e23913d68e04caec878a3560986a32a4
marcpre <marcus.presich@gmail.com>
2017-11-23 21:25:40 +0100

    fixing user data obj

--- src/t03-passport-local.js
+++ src/t03-passport-local.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const fs = require('fs')
 const path = require('path')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
@@ -8,9 +9,9 @@ const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
-//load user.json file
-const userObj = JSON.parse(path.join(__dirname, '/../data/user.json'))
-console.log(userObj)
+// load user.json file
+const d = fs.readFileSync(path.join(__dirname, '/../data/user.json'))
+const userObj = JSON.parse(d)
 
 const app = express()
 
@@ -20,61 +21,61 @@ app.set('view engine', 'pug')
 app.use(logger(process.env.LOG_ENV))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended: false,
+  extended: false,
 }))
 app.use(express.static(path.join(__dirname, '/../public')))
 app.use(cookieParser())
 
 app.use(session({
-    secret: 'super-mega-hyper-secret',
-    resave: false,
-    saveUninitialized: false,
+  secret: 'super-mega-hyper-secret',
+  resave: false,
+  saveUninitialized: false,
 }))
 app.use(passport.initialize())
 app.use(passport.session())
 
 function authenticate() {
-    
-    
-    passport.serializeUser((user, done) => {
-        done(null, user.id)
-      })
-      
-      passport.deserializeUser(async (id, done) => {
-//        const user = await serviceAuth.findById(id)
-        const user = userObj.find(item => item.id === id)
-        done(null, user)
-      })
-      
-      // Sign in with username and Password
-      passport.use('local', new LocalStrategy({
-        usernameField: 'username',
-      }, async(username, password, done) => {
-        const user = userObj.find(item => item.username === username)
-        done(null, user)
-      }))
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(async (id, done) => {
+    //        const user = await serviceAuth.findById(id)
+    const user = userObj.find(item => item.id === id)
+    done(null, user)
+  })
+
+  // Sign in with username and Password
+  passport.use('local', new LocalStrategy({
+    usernameField: 'username',
+  }, async(username, password, done) => {
+    const user = userObj.find(item => item.username === username)
+    done(null, user)
+  }))
 }
+
+authenticate()
 
 // routes
 app.get('/login', (req, res) => {
-    res.render('login')
+  res.render('login')
 })
 
 app.post('/login', (req, res, next) => {
-    passport.authenticate('local', (err, user) => {
-      if (err) return next(err)
-      if (!user) return res.status(401).json({ error: 'Email or password is incorrect.' })
-  
-      return res.render('dashboard')
-    })(req, res, next)
-  })
+  passport.authenticate('local', (err, user) => {
+    if (err) return next(err)
+    if (!user) return res.status(401).json({ error: 'Email or password is incorrect.' })
+
+    return res.render('dashboard')
+  })(req, res, next)
+})
 
 // Start Server
 const port = process.env.APP_PORT || 8080
 const host = process.env.APP_URL || 'localhost'
 
 app.listen(port, host, () => {
-    console.log(`Listening on ${host}:${port}`)
+  console.log(`Listening on ${host}:${port}`)
 })
 
 module.exports = app
